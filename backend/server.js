aad3e888f17b458244c42b19f9a9db36d6a9f4a5
Ishika Rawat <ishikarwt123@gmail.com>
2026-06-17 23:47:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ app.use(express.json());
 
 const DATA_DIR = path.join(__dirname, 'data');
 
-
 app.get('/airports', (req, res) => {
   try {
     const raw = fs.readFileSync(path.join(DATA_DIR, 'airports.json'), 'utf8');
