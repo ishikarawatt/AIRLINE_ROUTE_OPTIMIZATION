c272870710f0eb0c731af007e41d25f446fbfe0f
Ishika Rawat <ishikarwt123@gmail.com>
2026-06-17 23:47:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,6 @@ app.get('/airports', (req, res) => {
   }
 });
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Backend running at http://127.0.0.1:${PORT}`);
